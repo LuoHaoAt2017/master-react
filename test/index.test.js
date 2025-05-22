fe32ed7ab94578d1cb92da2f127cbf843949dda2
hao.luo01 <hao.luo01@ui-mh.com>
2025-05-22 14:38:33 +0800

    rxjs

---
+++ test/index.test.js
@@ -0,0 +1,69 @@
+import { describe, it, expect } from "vitest";
+import { from, switchMap } from 'rxjs';
+
+describe('rxjs', function () {
+  it('callback hell', () => {
+
+    function funcA(count, cb) {
+      if (count > 0) {
+        return cb(count + 1);
+      }
+      return count;
+    };
+
+    function funcB(count, cb) {
+      if (count > 0) {
+        return cb(count + 1);
+      }
+      return count;
+    };
+
+    function funcC(count, cb) {
+      if (count > 0) {
+        return cb(count + 1);
+      }
+      return count;
+    };
+
+    const result = funcA(1, (count1) => {
+      return funcB(count1, (count2) => {
+        return funcC(count2, (count3) => {
+          return count3;
+        });
+      });
+    });
+
+    expect(result).toBe(4);
+
+  });
+
+  it('pipe 改进', () => {
+    function funcA(count, cb) {
+      if (count > 0) return cb(count + 1);
+      return count;
+    }
+
+    function funcB(count, cb) {
+      if (count > 0) return cb(count + 1);
+      return count;
+    }
+
+    function funcC(count, cb) {
+      if (count > 0) return cb(count + 1);
+      return count;
+    }
+    // 将回调函数转换为返回 Observable 的形式
+    const rxFuncA = (count) => new Promise(resolve => funcA(count, resolve));
+    const rxFuncB = (count) => new Promise(resolve => funcB(count, resolve));
+    const rxFuncC = (count) => new Promise(resolve => funcC(count, resolve));
+
+    // 使用 switchMap 链式调用
+    // from 是 RxJS 中非常重要的创建操作符之一，用于从各种数据源创建 Observable。
+    from(rxFuncA(1)).pipe(
+      switchMap(count1 => rxFuncB(count1)),
+      switchMap(count2 => rxFuncC(count2))
+    ).subscribe(result => {
+      expect(result).toBe(4);
+    });
+  });
+});
