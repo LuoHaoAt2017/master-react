981dd18d7a1e86e6d9f23b9715ed82aff3ad7f56
hao.luo01 <hao.luo01@ui-mh.com>
2025-05-29 16:43:20 +0800

    rxjs 异步测试

--- test/creation.test.js
+++ test/creation.test.js
@@ -1,1 +1,18 @@
+import { describe, it, expect } from "vitest";
+import { isEqual } from "lodash";
 import { of } from "rxjs";
+
+describe('rxjs', function () {
+  it('hello rxjs', async () => {
+    const source$ = of(1, 2, 3);
+    const list = await new Promise(function(resolve, reject) {
+      let result = [];
+      source$.subscribe({
+        next: (value) => result.push(value),
+        complete: () => resolve(result),
+        error: (err) => reject(err)
+      });
+    });
+    expect(isEqual(list, [1, 2, 3])).toBeTruthy();
+  });
+});
---
+++ test/transformation.test.js
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe('rxjs', function () {
+  it('hello rxjs', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
