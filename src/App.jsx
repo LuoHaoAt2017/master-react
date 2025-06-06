9db0c9a36b8ffa51beca6cd60ad8d1b36bda4943
hao.luo01 <hao.luo01@ui-mh.com>
2025-06-06 10:46:56 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useRef } from 'react';
-import { map, fromEvent, merge, debounceTime, tap, filter, bufferTime, switchMap, timer, pairwise, buffer } from 'rxjs';
-
-/**
- * 左键单击 - 执行操作A
- * 右键单击 - 执行操作B
- * 左键双击 - 执行操作C
- * 右键双击 - 执行操作D
- * 在500ms内连续左右键交替点击 - 执行特殊操作E
- * 点击后3秒内没有其他操作 - 执行超时操作F
- */
-
-// 原生 dblclick 事件的间隔时间是系统级别的设定（通常约300-500ms），不可配置。
+import React, { useEffect, useRef, useState } from 'react';
+import { map, fromEvent, merge, debounceTime, tap, filter, bufferTime, switchMap, timer, pairwise, buffer, takeUntil } from 'rxjs';
 
 function App() {
   const btnRef = useRef();
+  const [count, setCount] = useState();
+  /**
+   * 左键单击 - 执行操作A
+   * 右键单击 - 执行操作B
+   * 左键双击 - 执行操作C
+   * 右键双击 - 执行操作D
+   * 在500ms内连续左右键交替点击 - 执行特殊操作E
+   * 点击后3秒内没有其他操作 - 执行超时操作F
+   */
+  // 原生 dblclick 事件的间隔时间是系统级别的设定（通常约300-500ms），不可配置。
+
   useEffect(() => {
     const leftClick$ = fromEvent(btnRef.current, 'click').pipe(filter(event => event.button === 0)).pipe(map(() => ({ type: 'left' })));
     const rightClick$ = fromEvent(btnRef.current, 'contextmenu').pipe(tap(event => event.preventDefault())).pipe(map(() => ({ type: 'right' })));
@@ -68,9 +68,22 @@ function App() {
     });
   }, []);
 
+
+  /**
+   * 统计5秒钟以内，鼠标点击按钮的次数
+   */
+  useEffect(() => {
+    const source$ = fromEvent(btnRef.current, 'click');
+    const countDown$ = timer(5000);
+    const target$ = source$.pipe(takeUntil(countDown$));
+    target$.subscribe(function() {
+      setCount(preCount => preCount + 1);
+    });
+  }, []);
+
   return (
     <button ref={btnRef}>
-      click
+      click {count}
     </button>
   )
 }
