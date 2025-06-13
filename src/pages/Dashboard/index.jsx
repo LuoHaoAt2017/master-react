b38bfb30dc8aa4d6466c8794085f8a34536a7c69
hao.luo01 <hao.luo01@ui-mh.com>
2025-06-13 16:51:28 +0800

    组件引用转发(Ref Forwarding)

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,5 +1,7 @@
-import { Button, Table } from "antd";
+import React, { useEffect, useRef } from "react";
+import { Button, Table, Input } from "antd";
 import withLoading from "@/hocs/withLoading";
+import withBorder from "@/hocs/withBorder";
 import { getDeviceListByOrg } from "@/services";
 import useDataFetcher from "@/hooks/useDataFetcher";
 
@@ -18,11 +20,26 @@ const columns = [
   }
 ]
 
+const OriginInput = React.forwardRef((props, ref) => <Input ref={ref} {...props} />);
+
+const BorderInput = withBorder(OriginInput);
+
 function Dashboard() {
+  const inputRef = useRef();
   const { data, loading, runAsync } = useDataFetcher(getDeviceListByOrg);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (inputRef && inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, 250);
+  }, []);
+
   return <div>
     <HocTable loading={loading} dataSource={data} columns={columns} />
     <Button onClick={runAsync} loading={loading}>Loading</Button>
+    <BorderInput ref={inputRef} value={"Hello World"} />
   </div>
 }
 
