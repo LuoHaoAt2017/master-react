7d7f20daf1373713548b516b343058a49a0b052f
hao.luo01 <hao.luo01@ui-mh.com>
2025-06-13 17:49:04 +0800

    暴露子组件方法给父组件

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef, forwardRef, useImperativeHandle } from "react";
 import { Button, Table, Input } from "antd";
 import withLoading from "@/hocs/withLoading";
 import withBorder from "@/hocs/withBorder";
@@ -20,12 +20,34 @@ const columns = [
   }
 ]
 
-const OriginInput = React.forwardRef((props, ref) => <Input ref={ref} {...props} />);
+const OriginInput = forwardRef((props, ref) => <Input ref={ref} {...props} />);
 
 const BorderInput = withBorder(OriginInput);
 
+const VideoPlayer = forwardRef((props, ref) => {
+  const videoRef = useRef();
+  const [playing, setPlaying] = useState(false);
+
+  useImperativeHandle(ref, () => ({
+    play() {
+      videoRef.current.play();
+      setPlaying(true);
+    },
+    pause() {
+      videoRef.current.pause();
+      setPlaying(false);
+    },
+    isPlaying: playing
+  }));
+
+  return <div>
+    <video ref={videoRef} src={props.src}></video>
+  </div>
+})
+
 function Dashboard() {
   const inputRef = useRef();
+  const playerRef = useRef();
   const { data, loading, runAsync } = useDataFetcher(getDeviceListByOrg);
 
   useEffect(() => {
@@ -40,6 +62,9 @@ function Dashboard() {
     <HocTable loading={loading} dataSource={data} columns={columns} />
     <Button onClick={runAsync} loading={loading}>Loading</Button>
     <BorderInput ref={inputRef} value={"Hello World"} />
+    <VideoPlayer ref={playerRef} src="movie.mp4" />
+    <Button onClick={() => playerRef.current.play()}>播放</Button>
+    <Button onClick={() => playerRef.current.pause()}>暂停</Button>
   </div>
 }
 
